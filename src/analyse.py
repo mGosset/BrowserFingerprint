3bb6fa6d703b6459e3c1b98befb7f7fe544c1a01
mGosset <marine.gosset.fr@gmail.com>
2018-04-05 11:34:53 +0200

    save work

--- src/analyse.py
+++ src/analyse.py
@@ -10,7 +10,7 @@ def get_number_distinct_values(cur, attributes):
 def get_number_missing_values(cur, attributes):
     res = dict()
     for attribute in attributes:
-        cur.execute("SELECT COUNT(DISTINCT(" + attribute + ")) as nb FROM extensionData WHERE " + attribute + " IS NOT NULL")
+        cur.execute("SELECT COUNT(DISTINCT(" + attribute + ")) as nb FROM extensionData WHERE " + attribute + " IS NULL")
         res_query = cur.fetchone()
         res[attribute] = res_query['nb']
 
