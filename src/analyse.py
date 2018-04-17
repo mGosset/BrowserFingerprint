05c7d4e46de9fa5963efb1ef6045c62ae28033e0
antoine  vastel <antoine.vastel@gmail.com>
2018-04-17 18:52:45 +0200

    adds entropy in analyze module
    Improve computation speed by using group by

--- src/analyse.py
+++ src/analyse.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 def get_number_distinct_values(cur, attributes):
     res = dict()
     for attribute in attributes:
@@ -6,6 +9,7 @@ def get_number_distinct_values(cur, attributes):
         res[attribute] = res_query['nb']
 
     return res
+
 
 def get_number_missing_values(cur, attributes):
     res = dict()
@@ -17,4 +21,39 @@ def get_number_missing_values(cur, attributes):
     return res
 
 
+def compute_entropy(df, attribute):
+    entropy = 0
+    tmp_stats = []
+    grouped = df.groupby(['id'])
+
+    for name, group in grouped:
+        different_values = group[attribute].unique()
+        tmp_stats += [{"id": name, attribute: value} for value in different_values]
+
+    df_stats = pd.DataFrame(tmp_stats)
+    serie = df_stats[attribute].value_counts()
+
+    if len(serie) == 1:
+        return 0
+
+    total_values = serie.sum()
+    for index, value in serie.iteritems():
+        pi = float(value) / float(total_values)
+        entropy += pi * np.log2(pi)
+
+    return -entropy
+
+
+def compute_normalized_entropy(df, attribute):
+    number_distinct = df[attribute].value_counts().sum()
+
+    if number_distinct == 1:
+        return 0
+
+    pi_worst = 1.0 / float(number_distinct)
+    worst_case = -number_distinct * pi_worst * np.log2(pi_worst)
+
+    df[attribute].value_counts().count()
+    entropy = compute_entropy(df, attribute)
+    return entropy / worst_case
 
